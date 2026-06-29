c269a2c222c80ce737ba9c7a8c944961c0322ca9
Josh Goodwin <j@joshuagoodw.in>
2026-06-29 10:39:14 +0100

    try working out swords express trip and service

--- vehicles/management/commands/swords_express.py
+++ vehicles/management/commands/swords_express.py
@@ -1,9 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 from django.contrib.gis.geos import GEOSGeometry
+from django.db.models import Q
 
 from busstops.models import Operator
+from bustimes.utils import get_calendars, get_routes
+from bustimes.models import Route, StopTime
 from ..import_live_vehicles import ImportLiveVehiclesCommand
 from ...models import VehicleLocation, VehicleJourney
 
@@ -16,6 +19,10 @@ def hyphenate(reg: str) -> str:
     while j < len(reg) and reg[j].isalpha():
         j += 1
     return f"{reg[:i]}-{reg[i:j]}-{reg[j:]}"
+
+
+def get_latlong(item):
+    return GEOSGeometry(f"POINT({item[2]} {item[1]})")
 
 
 class Command(ImportLiveVehiclesCommand):
@@ -55,9 +62,42 @@ class Command(ImportLiveVehiclesCommand):
         )
 
     def get_journey(self, item, _):
+        if len(item) < 3:
+            return
+
         journey = VehicleJourney()
+
+        when = self.get_datetime(item)
+        date = when.date()
+
+        routes = get_routes(
+            Route.objects.filter(
+                service__operator=self.operator, service__current=True
+            ),
+            date,
+        )
+        ten_minutes = timedelta(minutes=10)
+        now = timedelta(hours=when.hour, minutes=when.minute)
+        time_range = (now - ten_minutes, now + ten_minutes)
+
+        stop_times = StopTime.objects.filter(
+            Q(departure__range=time_range) | Q(arrival__range=time_range),
+            trip__route__in=routes,
+            trip__calendar__in=get_calendars(date),
+            stop__latlong__dwithin=(get_latlong(item), 0.01),
+        ).select_related("trip__route__service")
+
+        if stop_times:
+            if all(
+                stop_times[0].trip_id == stop_time.trip_id
+                for stop_time in stop_times[1:]
+            ):
+                journey.trip = stop_times[0].trip
+                journey.service = journey.trip.route.service
+                journey.route_name = journey.trip.route.line_name
+                journey.destination = journey.trip.headsign
         return journey
 
     def create_vehicle_location(self, item):
         if len(item) >= 3:
-            return VehicleLocation(latlong=GEOSGeometry(f"POINT({item[2]} {item[1]})"))
+            return VehicleLocation(latlong=get_latlong(item))
